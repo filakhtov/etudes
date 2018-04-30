22db3cb7a339937606516527b9800aa16b7de3a3
Garry Filakhtov <garry.filakhtov@bigcommerce.com>
2018-04-30 19:32:56 +1000

    2018-04-26

---
+++ index.js
@@ -0,0 +1,8 @@
+import moment from 'moment';
+
+console.log("Hello World");
+console.log(moment().startOf('day').fromNow());
+let name = "Bob";
+let time = "today";
+console.log(`Hello ${name}, how are you ${time}`);
+console.log("foo")
